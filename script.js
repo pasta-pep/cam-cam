33cf468dd79433e6668c63ff3003557861fbe685
pasta-pep <jcw.pep@gmail.com>
2026-09-18 23:48:13 -0400

    bigger halftone dots

--- script.js
+++ script.js
@@ -61,7 +61,7 @@ function applyEffect() {
     } else if (currentEffect === "film") {
         chain = chain.sepia(0.7).vignette(0.4, 0.6).noise(0.15);
     } else if (currentEffect === "halftone") {
-        chain = chain.colorHalftone(cx, cy, 0.75, 300).vibrance(1.0).brightnessContrast(0.05, 0.8);
+        chain = chain.colorHalftone(cx, cy, 0.75, 400).vibrance(1.0).brightnessContrast(0.05, 0.8);
     } else if (currentEffect === "comic") {
         chain = chain.dotScreen(cx, cy, 0, 3).brightnessContrast(0.1, 0.6);
     } else if (currentEffect === "motion") {
